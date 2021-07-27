f6535f0b3428bad31ddc5f03449c6e9bfaa5ebf5
calvin <calvin.francis96@outlook.com>
2021-07-27 19:07:42 -0400

    Done with assignment.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,11 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
+# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+require_relative "../app/models/artist.rb" 
